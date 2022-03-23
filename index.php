a4f670be0a30500b205ccbf1a498026dbdbbfc05
Matt <mattsantana97@gmail.com>
2022-03-23 15:52:01 -0300

    feito, cagado mas feito

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
+$html = file_get_contents("main.html");
+echo $html;
+
+$binaryId = ((int)$_POST["binary"]);
+
+$convert = bindec($binaryId);
+
+echo "<input id='binaryId'value='$convert'/>";
+
+
+
 
